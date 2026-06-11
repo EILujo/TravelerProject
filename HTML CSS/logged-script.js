3d47bd05bc7ef96ba8892edd9502a6df5f84dfe4
EILujo <157101341+EILujo@users.noreply.github.com>
2026-06-11 22:58:02 +0400

    Delete HTML CSS directory

--- HTML CSS/logged-script.js
+++
@@ -1,8 +0,0 @@
-window.onload = () => {
-	const savedImg = localStorage.getItem('userimg');
-	const navImg = document.getElementById('nav-profile-img');
-
-	if (savedImg && navImg) {
-		navImg.src = savedImg
-	}
-};
